497cf82f26b37c1b940de9d1c83129b560068ca7
Dimitris-Papadogiannakis <papadogd@gmail.com>
2022-09-16 06:22:49 +0300

    small things

--- FrontEndReact/recipes/src/App.js
+++ FrontEndReact/recipes/src/App.js
@@ -1,7 +1,7 @@
 import { React } from 'react';
 
 // Routing
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 // CSS
