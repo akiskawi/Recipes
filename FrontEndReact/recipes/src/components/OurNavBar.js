d9dc5309f3c984772e34d79f442ba685651247fc
Dimitris-Papadogiannakis <papadogd@gmail.com>
2022-09-16 06:20:02 +0300

    Navbar with Bootstrap in all views

---
+++ FrontEndReact/recipes/src/components/OurNavBar.js
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CollapsibleExample() {
+    return (
+        <div className="row">
+            <div className="col">
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand>Menu</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Link className='links' to="/">Home</Link>
+                            <Link className='links' to="login">Login</Link>
+                            <Link className='links' to="/register">Register</Link>
+                            <Link className='links' to="/createrecipe">Create Recipe</Link>
+                            <Link className='links' to="#">My Profile</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        </div>
+    );
+}
+
+export default CollapsibleExample;
--- FrontEndReact/recipes/src/components/views/NoPage.js
+++ FrontEndReact/recipes/src/components/views/NoPage.js
@@ -1,3 +1,5 @@
+import {React} from 'react'
+
 function NoPage(props) {
     return (
         <div>
