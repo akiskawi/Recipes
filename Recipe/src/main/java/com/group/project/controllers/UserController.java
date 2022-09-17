02e47d5b3406858398f72d829826e1e0b6b841f2
bourzan <andreasb20@yahoo.com>
2022-09-17 22:14:00 +0300

    ugly test on user crud from front end

--- Recipe/src/main/java/com/group/project/controllers/UserController.java
+++ Recipe/src/main/java/com/group/project/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.group.project.models.User;
 import com.group.project.services.UserServiceInterface;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Andreas
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 @RequestMapping("/users")
 public class UserController {
 
