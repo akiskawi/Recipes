35168d04b62a7038d390958341b7efe0e4f23f92
Yourwash C. Azakas <georgeazakas@outlook.com>
2022-09-19 19:46:28 +0300

    Chat Component

--- FrontEndReact/recipes/src/index.js
+++ FrontEndReact/recipes/src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import AppYW from './AppYW';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
